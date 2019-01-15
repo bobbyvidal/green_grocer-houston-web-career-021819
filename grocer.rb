b4cb5c288d990fc8f8302a11992abaeff7ac05c7
Robert Vidal <bobbyvidal@gmail.com>
2019-01-15 00:46:18 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,17 @@
 def consolidate_cart(cart)
   # code here
+  consolidated = {}
+  cart.each do |item|
+  item.each do |key, value|
+    if consolidated.has_key?(key)
+      consolidated[key][:count] += 1
+    else
+      consolidated[key] = value
+      consolidated[key][:count] = 1
+    end
+  end
+end
+consolidated
 end
 
 def apply_coupons(cart, coupons)
